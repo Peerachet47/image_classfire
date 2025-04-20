07211524887033684f6faade6dc37ff6d06a2670
Peerachet K. <167800654+Peerachet47@users.noreply.github.com>
2025-04-20 15:41:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Created on Sun Apr 20 13:52:59 2025
 """
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predictions, preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import decode_predictions, preprocess_input
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
