497a89885bb860a713ad8bbf3ff1aad51acaf406
Peerachet K. <167800654+Peerachet47@users.noreply.github.com>
2025-04-20 15:25:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,34 +11,36 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predi
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
+import io
 import pickle
 
 #load model
-with open ('model.plk','rb') as f:
+with open ('model.pkl','rb') as  f:
     model = pickle.load(f)
     
 #set title application
 st.title("Image Classification with MobileNetV2 by Peerachet Khanitson 6531501092")
 
 #flie upload
-upload_file = st.file_uploader("Upload image:", type=["jpg","jpeg","png"])
+uploaded_file=st.file_uploader("Upload image :", type=["jpg","jpeg","png"])
 
-if upload_file is not None:
-    #display image on screen
-    img = Image.open(upload_file)
-    st.image(img, caption="Upload Image")
+if uploaded_file is not None:
+    # Display image
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image")
     
     #preproccessing
-    img = img.resize ((224,224))
+    img = img.resize((224, 224))
     x = image.img_to_array(img)
-    x = np.expand_dims(x,axis=0)
+    x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     
-    #prediction
-    preds = model.predic(x)
+    # Prediction
+    preds = model.predict(x)
     top_preds = decode_predictions(preds, top=3)[0]
-    
-    #display priediction
-    st.subheader("Prediction:")
-    for i,pred in enumerate(top_preds):
-        st.write(f"{i+1}. **{pred[1]}** - {round(pred[2]*100,2)}%")
+
+    # Display predictions
+    st.subheader("Predictions:")
+    for i, pred in enumerate(top_preds):
+        st.write(f"{i+1}. {pred[1]} - {round(pred[2]*100, 2)}%")
+
